f86498be599015a647d0d632aa5763d7a5adb481
angelovlima <angelovictor2001@hotmail.com>
2021-08-03 10:05:48 -0300

    Projeto criado

---
+++ src/main/java/com/fatec/backendtopicosespeciais/BackEndTopicosEspeciaisApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.backendtopicosespeciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTopicosEspeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTopicosEspeciaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/backendtopicosespeciais/BackEndTopicosEspeciaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.backendtopicosespeciais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTopicosEspeciaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
